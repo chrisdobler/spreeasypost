02c9d17a3e392fd7020e7f26a32bb9826d12aef4
Christopher Dobler <chrisdobler@gmail.com>
2017-01-25 21:34:45 -0800

    Adds rspec tests for button

--- spec/features/admin/orders/shipments_spec.rb
+++ spec/features/admin/orders/shipments_spec.rb
@@ -14,9 +14,33 @@ describe "Shipments", type: :feature, js: true do
       end
     end
 
-    it "should have a button to purchase postage" do
-      expect(page).to have_link("Purchase Postage")    
+    describe "purchase postage button" do
+      
+      it "should exist" do
+        expect(page).to have_link("Purchase Postage")    
+      end
+
+      it "on click should call method using ajax" do
+        WebMock.enable!
+        binding.pry
+        stub_request(:post, '/easy_post/shipment/'+order.id+'/postage').
+        with(body: "abc", headers: { 'Content-Length' => 3 })
+        # should have_link('Purchase Postage', href: spree.new_easy_post_shipment_postage_path(order.shipments[0].id))
+        click_on "Purchase Postage"
+        wait_for_ajax
+        expect(page).to have_content("abc") 
+
+        WebMock.disable!
+      end
     end
-    
+
+    # it "can ship a completed order" do
+    #   click_on "Ship"
+    #   wait_for_ajax
+
+    #   expect(page).to have_content("shipped package")
+    #   expect(order.reload.shipment_state).to eq("shipped")
+    # end
+
   end
 end

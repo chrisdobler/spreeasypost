c02646612bcd53dcde391facedbb5241421c59e2
Christopher Dobler <chrisdobler@gmail.com>
2017-01-25 21:30:23 -0800

    Adds Postage controller

--- app/controllers/spree/easy_post/postage_controller.rb
+++ app/controllers/spree/easy_post/postage_controller.rb
@@ -1,6 +1,14 @@
 class Spree::EasyPost::PostageController < ApplicationController
 
-	def purchase
+  require 'easypost'
+
+	def create
+		params.each do |key,value|
+		  Rails.logger.warn "Param #{key}: #{value}"
+		end
+		respond_to do |format|
+      format.json { render :json => params[:shipment_id] }
+    end
 	end
 
 end

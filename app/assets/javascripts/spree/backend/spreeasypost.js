5095d450ccac2071258c7f64aee37c49a6783b9c
Christopher Dobler <chrisdobler@gmail.com>
2017-01-26 04:37:58 -0800

    Updates button logic

--- app/assets/javascripts/spree/backend/spreeasypost.js
+++ app/assets/javascripts/spree/backend/spreeasypost.js
@@ -6,18 +6,18 @@ $(document).ready(function () {
   $('[data-hook=admin_shipment_form] a.purchase-postage').on('click', function () {
     var link = $(this);
     var shipment_number = link.data('shipment-number');
-    var url = Spree.url('/easy_post/shipment/'+shipment_number+'/postage');
+    var url = Spree.url('/easy_post/shipment/'+shipment_number+'/postage.json');
     $.ajax({
       type: 'POST',
       url: url,
       data: {
         token: Spree.api_key
       }
-    }).success(function () {
-      window.location.reload();
+    }).done(function (data) {
+      console.log(data.url);
+      window.location.assign(data.url);
     }).error(function (msg) {
       console.log(msg);
-      alert("hfuc")
     });
   });
 });
--- app/controllers/spree/easy_post/postage_controller.rb
+++ app/controllers/spree/easy_post/postage_controller.rb
@@ -6,8 +6,9 @@ class Spree::EasyPost::PostageController < ApplicationController
 		params.each do |key,value|
 		  Rails.logger.warn "Param #{key}: #{value}"
 		end
+		response = {url: "https://easypost-files.s3-us-west-2.amazonaws.com/files/postage_label/20170126/2baff2c5684648ab8b8b472bcde66802.png", id: params[:shipment_id] }
 		respond_to do |format|
-      format.json { render :json => params[:shipment_id] }
+      format.json { render :json => response }
     end
 	end
 
